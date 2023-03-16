35a02bbc335e1631edf62806b25bcbbc90b1a36d
vikash75120 <113852053+vikash75120@users.noreply.github.com>
2023-03-17 02:10:41 +0530

    commit1

---
+++ src/components/HomeContent.jsx
@@ -0,0 +1,44 @@
+const Home = () => {
+  return (
+    <div className="mainBox">
+      <div className="box12">
+      <div className="box1 h-0">
+        <h2>Play</h2>
+        <small>Let's play</small>
+        
+      </div>
+
+      <div className="box2">
+      <h2>Chess World</h2>
+        <small>Let's play</small>
+
+      </div>
+      </div>
+
+      <div className="boxcontainer">
+      <div className="box3">
+        <div className="box31">
+        <h2>Learn</h2>
+        <small>Learn more</small>
+
+        </div>
+        <div className="box32">
+        <h2>About</h2>
+        <small>Let's play</small>
+
+        </div>
+        
+      </div>
+      <div className="box33">
+      <h2>Something</h2>
+        <small>Know about us</small>
+
+        </div>
+      </div>
+      
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+import HomeContent from '../components/HomeContent'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Navbar/>
+      <HomeContent/>
+    </div>
+  )
+}
